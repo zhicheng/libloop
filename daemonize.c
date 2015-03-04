ec94d25779aa0a1bf07981df9d5fffb4339334d8
Zhicheng Wei <zhicheng@opensourceforge.net>
2015-03-04 15:21:38 +0800

    add daemonize.c

---
+++ daemonize.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/resource.h>
+
+#include "die.h"
+
+/*
+ * ref:
+ * http://0pointer.de/public/systemd-man/daemon.html
+ */
+void
+daemonize(int nochdir, int noclose)
+{
+	int   sig;
+	pid_t pid;
+	sigset_t set;
+	struct rlimit limit;
+
+	if (!noclose) {
+		/*
+		 * Close all open file descriptors except STDIN, STDOUT, STDERR
+		 * (i.e. the first three file descriptors 0, 1, 2).
+		 * This ensures that no accidentally passed file descriptor stays around
+		 * in the daemon process.
+		 */
+		if (getrlimit(RLIMIT_NOFILE, &limit) == -1) {
+			die("getrlimit %s", strerror(errno));
+		}
+
+		for(; limit.rlim_cur > 3; limit.rlim_cur--) {
+			close(limit.rlim_cur - 1);
+		}
+	}
+
+	/* Reset all signal handlers to their default. */
+	for (sig = 1; sig <= NSIG; sig++) {
+		signal(sig, SIG_DFL);
+	}
+
+	/* Reset the signal mask using sigprocmask(). */
+	sigemptyset(&set);
+	sigprocmask(SIG_SETMASK, &set, NULL);
+
+	/* Call fork(), to create a background process. */
+	pid = fork();
+	if (pid == -1) {
+		die("fork %s", strerror(errno));
+	} else if (pid != 0) {
+		_exit(0);
+	}
+
+	/* Call setsid() to detach from any terminal and create an independent session. */
+	if (setsid() == -1) {
+		die("setsid %s", strerror(errno));
+	}
+
+	/* Call fork() again, to ensure that the daemon can never re-acquire a terminal again. */
+	signal(SIGHUP, SIG_IGN);
+	pid = fork();
+	if (pid == -1) {
+		die("fork %s", strerror(errno));
+	} else if (pid != 0) {
+		_exit(0);
+	}
+
+	/*  connect /dev/null to STDIN, STDOUT, STDERR. */
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+
+	if (open("/dev/null", O_RDONLY) == -1) {
+		die("open %s", strerror(errno));
+	}
+	if (open("/dev/null", O_WRONLY) == -1) {
+		die("open %s", strerror(errno));
+	}
+	if (open("/dev/null", O_RDWR) == -1) {
+		die("open %s", strerror(errno));
+	}
+
+	/*
+	 * reset the umask to 0, so that the file modes passed to
+	 * open(), mkdir() and suchlike directly control the access mode
+	 * of the created files and directories.
+	 */
+	umask(0);
+
+	if (!nochdir) {
+		/*
+		 * change the current directory to the root directory (/),
+		 * in order to avoid that the daemon involuntarily blocks
+		 * mount points from being unmounted.
+		 */
+		if (chdir("/") == -1) {
+			die("chdir %s", strerror(errno));
+		}
+	}
+}
---
+++ daemonize.h
@@ -0,0 +1,7 @@
+#ifndef __DAEMONIZE_H__
+#define __DAEMONIZE_H__
+
+void
+daemonize(int nochdir, int noclose);
+
+#endif /* __DAEMONIZE_H__ */
